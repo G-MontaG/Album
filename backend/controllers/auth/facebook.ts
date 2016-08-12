10ad4d1cb0ada6b2ba8bcb295853b8cd45ea381d
Arthur Osipenko <arthur.osipenko@gmail.com>
2016-08-12 18:54:18 +0300

    facebook handler refactoring

--- backend/controllers/auth/facebook.ts
+++ backend/controllers/auth/facebook.ts
@@ -14,7 +14,7 @@ export function facebookCodeHandler(req: express.Request, res: express.Response)
   res.send({redirectUrl: getFacebookCodeUrl});
 }
 
-export function facebookTokenHandler(req:RequestWithAuthSession, res: express.Response) {
+export function facebookTokenHandler(req: RequestWithAuthSession, res: express.Response) {
   if (req.query.error) {
     ServerMessage.error(req, res, 401, 'Facebook authentication error');
     console.error('Facebook authentication error');
@@ -32,74 +32,65 @@ export function facebookTokenHandler(req:RequestWithAuthSession, res: express.Re
           resolve(JSON.parse(data));
         });
       });
-      tokenReq.on('error', (err) => {
-        ServerMessage.error(req, res, 401, 'Facebook authentication error. Can not get token');
+      tokenReq.on('error', () => {
+        let err: ServerError = new Error("Facebook authentication error. Can not get token");
+        err.status = 401;
         reject(err);
       });
     }).then((authData: AuthData) => {
-      new Promise((resolve, reject) => {
-        let userDataReq = https.get(`https://graph.facebook.com/v2.6/me?fields=id,email,first_name,last_name,gender,picture,locale&access_token=${authData.access_token}`, (resUser) => {
-          let data = '';
-          resUser.on('data', (chunk) => {
-            data += chunk;
-          });
-          resUser.on('end', () => {
-            req.session.facebookUserData = JSON.parse(data);
-            res.redirect('http://127.0.0.1:3000/facebook-auth/response');
-            resolve();
-          });
+      let userDataReq = https.get(`https://graph.facebook.com/v2.6/me?fields=id,email,first_name,last_name,gender,picture,locale&access_token=${authData.access_token}`, (resUser) => {
+        let data = '';
+        resUser.on('data', (chunk) => {
+          data += chunk;
         });
-        userDataReq.on('error', (err) => {
-          ServerMessage.error(req, res, 401, 'Facebook authentication error. Can not get user info');
-          reject(err);
+        resUser.on('end', () => {
+          req.session.facebookUserData = JSON.parse(data);
+          res.redirect('http://127.0.0.1:3000/facebook-auth/response');
         });
-      }).catch((err) => {
-        console.error(err);
       });
+      userDataReq.on('error', () => {
+        let err: ServerError = new Error("Facebook authentication error. Can not get user info");
+        err.status = 401;
+        throw err;
+      });
+      return userDataReq;
     }).catch((err) => {
-      console.error(err);
+      ServerMessage.error(req, res, err.status || 500, err.message || 'Mongo database find user error');
+      return err;
     });
   }
 }
 
-export function facebookUserHandler(req:RequestWithAuthSession, res: express.Response) {
-  new Promise((resolve, reject) => {
-    User.findOne({email: req.session.facebookUserData.email}, (err, user) => {
-      if (err) {
-        ServerMessage.error(req, res, 500, 'Mongo database error');
-        reject(err);
-      }
-      if (!user) {
-        let _data = {
-          email: req.session.facebookUserData.email,
-          password: '',
-          profile: {
-            firstname: req.session.facebookUserData.first_name,
-            lastname: req.session.facebookUserData.last_name,
-            gender: req.session.facebookUserData.gender,
-            picture: {
-              url: req.session.facebookUserData.picture.data.url,
-              source: 'facebook'
-            },
-            language: req.session.facebookUserData.locale
-          }
-        };
-        let newUser = new User(_data);
-        delete req.session.facebookUserData;
-        delete _data.email;
-        _data.password = newUser.createPassword();
-        cs.generateEmailToken(newUser, 'verify');
-        newUser.cryptPassword().then(() => {
-          newUser.save((err, user) => {
-            if (err) {
-              ServerMessage.error(req, res, 500, 'Mongo database error');
-              reject(err);
-            }
-            let mailOptions = {
-              to: user.email,
-              from: 'arthur.osipenko@gmail.com',
-              subject: 'Hello on XXX',
-              text: `Hello. This is credentials for your account.
+export function facebookUserHandler(req: RequestWithAuthSession, res: express.Response) {
+  User.findOne({email: req.session.facebookUserData.email}).exec().then((user) => {
+    if (!user) {
+      let _data = {
+        email: req.session.facebookUserData.email,
+        password: '',
+        profile: {
+          firstname: req.session.facebookUserData.first_name,
+          lastname: req.session.facebookUserData.last_name,
+          gender: req.session.facebookUserData.gender,
+          picture: {
+            url: req.session.facebookUserData.picture.data.url,
+            source: 'facebook'
+          },
+          language: req.session.facebookUserData.locale
+        }
+      };
+      let newUser = new User(_data);
+      delete req.session.facebookUserData;
+      delete _data.email;
+      _data.password = newUser.createPassword();
+      cs.generateEmailToken(newUser, 'verify');
+      newUser.cryptPassword().then(() => {
+        return newUser.save().exec();
+      }).then((user) => {
+        let mailOptions = {
+          to: user.email,
+          from: 'arthur.osipenko@gmail.com',
+          subject: 'Hello on XXX',
+          text: `Hello. This is credentials for your account.
                     You no need google account every time. You can use this
                     Email: ${user.email}
                     Password: ${_data.password}
@@ -107,56 +98,54 @@ export function facebookUserHandler(req:RequestWithAuthSession, res: express.Res
                     This is a token for your account 
                     ${user.emailVerifyToken.value.slice(0, cs.emailTokenLength / 2)} ${user.emailVerifyToken.value.slice(cs.emailTokenLength / 2, cs.emailTokenLength)}
                     Please go back and enter it in your profile to verify your email.`
-            };
-            delete _data.password;
-            cs.transporter.sendMail(mailOptions, function (err) {
-              if (err) {
-                ServerMessage.error(req, res, 500, 'Send email error');
-                reject(err);
-              }
-              return user;
-            });
-          }).then((user) => {
-            // if you keep in token sensitive info encrypt it before use jwt.sign()
-            let _token = jwt.sign({
-              id: user._id,
-              'user-agent': req.headers['user-agent']
-            }, process.env.JWT_SECRET, {
-              algorithm: cs.tokenAlg,
-              expiresIn: `${cs.tokenExp}d`,
-              jwtid: process.env.JWT_ID
-            });
-            ServerMessage.message(res, 200, {message: 'User is authorized', token: _token});
-            resolve();
-          }).catch((err) => {
-            console.error(err);
-          });
-        });
-      } else {
-        if (user.profile.picture.source === 'facebook') {
-          user.picture = req.session.facebookUserData.picture.data.url;
-        }
-        user.save((err, user) => {
-          delete req.session.facebookUserData;
+        };
+        delete _data.password;
+        cs.transporter.sendMail(mailOptions, function (err) {
           if (err) {
-            ServerMessage.error(req, res, 500, 'Mongo database error');
-            reject(err);
+            let err: ServerError = new Error("Send email error");
+            err.status = 500;
+            throw err;
           }
-          // if you keep in token sensitive info encrypt it before use jwt.sign()
-          let _token = jwt.sign({
-            id: user._id,
-            'user-agent': req.headers['user-agent']
-          }, process.env.JWT_SECRET, {
-            algorithm: cs.tokenAlg,
-            expiresIn: `${cs.tokenExp}d`,
-            jwtid: process.env.JWT_ID
-          });
-          ServerMessage.message(res, 200, {message: 'User is authorized', token: _token});
-          resolve();
+          return user;
         });
+      }).then((user) => {
+        // if you keep in token sensitive info encrypt it before use jwt.sign()
+        let _token = jwt.sign({
+          id: user._id,
+          'user-agent': req.headers['user-agent']
+        }, process.env.JWT_SECRET, {
+          algorithm: cs.tokenAlg,
+          expiresIn: `${cs.tokenExp}d`,
+          jwtid: process.env.JWT_ID
+        });
+        ServerMessage.message(res, 200, {message: 'User is authorized', token: _token});
+      }).catch((err) => {
+        ServerMessage.error(req, res, err.status || 500, err.message || 'Mongo database save user error');
+        return err;
+      });
+    } else {
+      if (user.profile.picture.source === 'facebook') {
+        user.picture = req.session.facebookUserData.picture.data.url;
       }
-    });
+      user.save().exec().then((user) => {
+        delete req.session.facebookUserData;
+        // if you keep in token sensitive info encrypt it before use jwt.sign()
+        let _token = jwt.sign({
+          id: user._id,
+          'user-agent': req.headers['user-agent']
+        }, process.env.JWT_SECRET, {
+          algorithm: cs.tokenAlg,
+          expiresIn: `${cs.tokenExp}d`,
+          jwtid: process.env.JWT_ID
+        });
+        ServerMessage.message(res, 200, {message: 'User is authorized', token: _token});
+      }).catch((err) => {
+        ServerMessage.error(req, res, err.status || 500, err.message || 'Mongo database find user error');
+        return err;
+      });
+    }
   }).catch((err) => {
-    console.error(err);
+    ServerMessage.error(req, res, err.status || 500, err.message || 'Mongo database find user error');
+    return err;
   });
 }
