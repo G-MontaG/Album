c88485ccf4808af14c49f18b4e679522e0e3a4dd
Arthur Osipenko <arthur.osipenko@gmail.com>
2016-08-12 18:23:24 +0300

    forgot handler refactoring

--- backend/controllers/auth/forgot.ts
+++ backend/controllers/auth/forgot.ts
@@ -5,6 +5,7 @@ import jwt = require('jsonwebtoken');
 
 import * as cs from './constants';
 const User = require('../../model/user');
+import {UserType} from '../../model/user';
 import {ServerMessage} from '../../helpers/serverMessage';
 import {RequestWithAuthSession, AuthData} from "./requestSession";
 
@@ -16,46 +17,37 @@ export function forgotPasswordEmailHandler(req: express.Request, res: express.Re
     ServerMessage.error(req, res, 401, errors[0].msg);
   } else {
     let _data = req.body.data;
-    new Promise((resolve, reject) => {
-      User.findOne({email: _data.email}, (err, user) => {
-        if (err) {
-          ServerMessage.error(req, res, 500, 'Mongo database error');
-          reject(err);
-        }
-
-        if (!user) {
-          ServerMessage.error(req, res, 401, 'Email not found');
-          reject();
-        } else {
-          cs.generateEmailToken(user, 'forgot');
-          user.save((err) => {
-            if (err) {
-              ServerMessage.error(req, res, 500, 'Mongo database error');
-              reject(err);
-            }
-            let mailOptions = {
-              to: user.email,
-              from: 'arthur.osipenko@gmail.com',
-              subject: 'Forgot password',
-              text: `Hello. This is a token for your account 
-              ${user.forgotPasswordToken.value.slice(0, cs.emailTokenLength / 2)} ${user.forgotPasswordToken.value.slice(cs.emailTokenLength / 2, cs.emailTokenLength)}
+    let currentUser: UserType;
+    User.findOne({email: _data.email}).exec().then((err, user) => {
+      if (!user) {
+        let err: ServerError = new Error("Email not found");
+        err.status = 401;
+        throw err;
+      } else {
+        cs.generateEmailToken(user, 'forgot');
+        currentUser = user;
+        return currentUser.save().exec();
+      }
+    }).then(() => {
+      let mailOptions = {
+        to: currentUser.email,
+        from: 'arthur.osipenko@gmail.com',
+        subject: 'Forgot password',
+        text: `Hello. This is a token for your account 
+              ${currentUser.forgotPasswordToken.value.slice(0, cs.emailTokenLength / 2)} ${currentUser.forgotPasswordToken.value.slice(cs.emailTokenLength / 2, cs.emailTokenLength)}
               Please go back and enter it in forgot password form.`
-            };
-            cs.transporter.sendMail(mailOptions, function (err) {
-              if (err) {
-                ServerMessage.error(req, res, 500, 'Send email error');
-                reject(err);
-              }
-              ServerMessage.message(res, 200, {message: 'Token has been sent', flag: true});
-              resolve();
-            });
-          }).catch((err) => {
-            console.error(err);
-          });
+      };
+      cs.transporter.sendMail(mailOptions, function (err) {
+        if (err) {
+          let err: ServerError = new Error("Send email error");
+          err.status = 500;
+          throw err;
         }
+        ServerMessage.message(res, 200, {message: 'Token has been sent', flag: true});
       });
     }).catch((err) => {
-      console.error(err);
+      ServerMessage.error(req, res, err.status || 500, err.message || 'Mongo database find user error');
+      return err;
     });
   }
 }
@@ -81,40 +73,32 @@ export function forgotPasswordTokenHandler(req: RequestWithAuthSession, res: exp
     } else {
       req.session.forgotTokenAttempts++;
       let _data = req.body.data;
-      new Promise((resolve, reject) => {
-        User.findOne({'forgotPasswordToken.value': _data.token}, (err, user) => {
-          if (err) {
-            ServerMessage.error(req, res, 500, 'Mongo database error');
-            reject(err);
-          }
-          if (!user) {
-            ServerMessage.error(req, res, 401, 'Token not found');
-            reject();
-          } else if (moment() > user.forgotPasswordToken.exp) {
-            ServerMessage.error(req, res, 401, 'Token has expired');
-            reject();
-          } else {
-            user.forgotPasswordToken = undefined;
-            user.save((err) => {
-              if (err) {
-                ServerMessage.error(req, res, 500, 'Mongo database error');
-                reject(err);
-              }
-              let _token = jwt.sign({
-                id: user._id,
-                'user-agent': req.headers['user-agent']
-              }, process.env.JWT_SECRET, {
-                algorithm: cs.tokenAlg,
-                expiresIn: `${cs.tokenExp}d`,
-                jwtid: process.env.JWT_ID
-              });
-              ServerMessage.message(res, 200, {message: 'Token is valid', flag: true, token: _token});
-              resolve();
-            });
-          }
+      User.findOne({'forgotPasswordToken.value': _data.token}).exec().then((user) => {
+        if (!user) {
+          let err: ServerError = new Error("Token not found");
+          err.status = 401;
+          throw err;
+        } else if (moment() > user.forgotPasswordToken.exp) {
+          let err: ServerError = new Error("Token has expired");
+          err.status = 401;
+          throw err;
+        } else {
+          delete user.forgotPasswordToken;
+          return user.save().exec();
+        }
+      }).then((user) => {
+        let _token = jwt.sign({
+          id: user._id,
+          'user-agent': req.headers['user-agent']
+        }, process.env.JWT_SECRET, {
+          algorithm: cs.tokenAlg,
+          expiresIn: `${cs.tokenExp}d`,
+          jwtid: process.env.JWT_ID
         });
+        ServerMessage.message(res, 200, {message: 'Token is valid', flag: true, token: _token});
       }).catch((err) => {
-        console.error(err);
+        ServerMessage.error(req, res, err.status || 500, err.message || 'Mongo database find user error');
+        return err;
       });
     }
   }
@@ -130,34 +114,24 @@ export function forgotPasswordNewPasswordHandler(req: RequestWithAuthSession, re
     ServerMessage.error(req, res, 401, errors[0].msg);
   } else {
     let _data = req.body.data;
-    new Promise((resolve, reject) => {
-      User.findOne({_id: req.userId}, (err, user) => {
-        if (err) {
-          ServerMessage.error(req, res, 500, 'Mongo database error');
-          reject(err);
-        }
-        if (!user) {
-          ServerMessage.error(req, res, 401, 'User not found');
-          reject();
-        } else {
-          user.password = _data.password;
-          delete _data.password;
-          user.cryptPassword().then(() => {
-            user.save((err) => {
-              if (err) {
-                ServerMessage.error(req, res, 500, 'Mongo database error');
-                reject(err);
-              }
-              ServerMessage.message(res, 200, {message: 'Password has been changed', flag: true});
-              resolve();
-            });
-          }).catch((err) => {
-            console.error(err);
-          });
-        }
-      });
+    let currentUser: UserType;
+    User.findOne({_id: req.userId}).exec().then((user) => {
+      if (!user) {
+        let err: ServerError = new Error("User not found");
+        err.status = 401;
+        throw err;
+      } else {
+        user.password = _data.password;
+        delete _data.password;
+        return user.cryptPassword();
+      }
+    }).then(() => {
+      return currentUser.save().exec();
+    }).then(() => {
+      ServerMessage.message(res, 200, {message: 'Password has been changed', flag: true});
     }).catch((err) => {
-      console.error(err);
+      ServerMessage.error(req, res, err.status || 500, err.message || 'Mongo database find user error');
+      return err;
     });
   }
 }
